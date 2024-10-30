4b88685c703ab628034d0df5462361f442b0eae4
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-30 17:20:58 +0100

    working on structure

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-console.log("Hi");
 const express = require('express');
 const app = express();
 const host = '127.0.0.1';
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Example app listening on http://${host}:${port}`);
+    console.log(`My app listening on http://${host}:${port}`);
   });
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Benvenuto nel mio blog!</h1>')
 });
-
-
