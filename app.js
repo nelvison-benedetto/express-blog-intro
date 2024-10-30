d9f70071ca644e7b8b75f0fc81de8147e4572531
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-30 18:04:16 +0100

    working on structure

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+//console.log(process.env.DB_USER)  //print nelvison
+
 const express = require('express');
 const app = express();
 const host = '127.0.0.1';
 const port = 3000;
 app.listen(port, () => {
-    console.log(`My app listening on http://${host}:${port}`);
+    console.log(`Server running on http://${host}:${port}`);
   });
 app.get('/', (req, res) => {
   res.send('<h1>Benvenuto nel mio blog!</h1>')
 });
+
+
